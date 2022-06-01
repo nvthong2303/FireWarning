438f52d3dab9ec54c6991a67ad10d43c94a5cf30
nvthong2303 <nvthong2303@gmail.com>
2022-06-02 00:56:20 +0700

    delete client error

--- client/src/components/Banner/Banner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import banner from '../../assets/smartBuilding.jpg';
-
-export default function Banner() {
-  return (
-    <img src={banner} className="col-md-6 p-0" alt="banner" />
-  )
-}
--- client/src/guards/auth.ts
+++
@@ -1,6 +0,0 @@
-const auth = () => {
-    const token = localStorage.getItem('x_access_token');
-    return token;
-}
-
-export default auth;
